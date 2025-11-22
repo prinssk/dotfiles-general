1a98069281e4521ff2a993dcf9128a8c25234a0e
prinssk <66834265+prinssk@users.noreply.github.com>
2025-11-22 03:00:22 +0000

    screenshot and add to clipboard

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,9 +3,12 @@
 dir="$HOME/ss"
 mkdir -p "$dir"
 
-file="$dir/chop_$(date +%Y_%m_$d_%S).png"
+file="$dir/chop_$(date +%Y_%m_$d__%H_%M_%S).png"
 
 region="$(slurp)"
 [ -z "$region" ] && exit 0   # user cancelled
 
 grim -g "$region" "$file"
+
+wl-copy < "$file"
+cliphist store < "$file"
